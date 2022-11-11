68ac30da74b05ae85f930927d5bcd315089e6cf8
yrysd <alymyryskeldi@gmail.com>
2022-11-11 14:55:17 +0300

    Initial commit

---
+++ src/Circle/Circle.java
@@ -0,0 +1,13 @@
+package Circle;
+
+public class Circle {
+
+    public static void area(int radius){
+        System.out.println(Math.PI * (radius * radius));
+
+    }
+    public static void circumference(int radius){
+        System.out.println(Math.PI * 2 * radius);
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import Circle.Circle;
+
+import java.util.concurrent.Callable;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Circle.area(4);
+
+        Circle.circumference(5);
+    }
+}
